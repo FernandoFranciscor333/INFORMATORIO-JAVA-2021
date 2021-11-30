50ad7e742f785e0dc7fdf2f1f06c05541b7dfd78
fernandofranciscor333 <fernandofranciscor333hotmail.com.ar>
2021-11-30 19:08:59 -0300

    correccion

--- Ejercicios Complementarios/EjerciciosComplementariosLevel3/Ejercicio5/Ejercicio5.java
+++ Ejercicios Complementarios/EjerciciosComplementariosLevel3/Ejercicio5/Ejercicio5.java
@@ -2,6 +2,9 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class Ejercicio5 {
 
@@ -9,11 +12,11 @@ public class Ejercicio5 {
 
         //Input
         List<Alumnos> alumnosLista = List.of(
-            new Alumnos("Homero", "Simpson", LocalDate.now().minusYears(30)),
-            new Alumnos("Cosme", "Fulanito", LocalDate.now().minusYears(28)),
-            new Alumnos("Juan", "Perez", LocalDate.now().minusYears(25)),
-            new Alumnos("Andrea", "Gomez", LocalDate.now().minusYears(25)),
-            new Alumnos("Maria", "Coso", LocalDate.now().minusYears(22))            
+            new Alumnos("Homero", "Simpson", LocalDate.parse("2021-11-28").minusYears(30)),
+            new Alumnos("Cosme", "Fulanito", LocalDate.parse("2021-10-18").minusYears(28)),
+            new Alumnos("Juan", "Perez", LocalDate.parse("2021-04-12").minusYears(25)),
+            new Alumnos("Andrea", "Gomez", LocalDate.parse("2021-03-09").minusYears(25)),
+            new Alumnos("Maria", "Coso", LocalDate.parse("2021-01-20").minusYears(22))            
         ); 
 
         //Output
@@ -22,16 +25,11 @@ public class Ejercicio5 {
 
     public static void mapeador(List<Alumnos> lista){
 
-        HashMap<String, Integer> mapa = new HashMap<String, Integer>();
-        LocalDate fechaActual = LocalDate.parse("2021-11-28");        
+        Map<String, Integer> mapa = new HashMap<String, Integer>();
+        LocalDate fechaActual = LocalDate.now();         
 
-        for (Alumnos alumno:lista){
-            String nombreCompleto = alumno.getApellido()+" "+alumno.getNombre();
-            Period diferenciaFechas = alumno.getFechaDeNacimiento().until(fechaActual);
-            Integer edad = diferenciaFechas.getYears();
-
-            mapa.put(nombreCompleto, edad);
-        }
+        mapa = lista.stream().collect(Collectors.toMap(x -> x.getApellido()+" "+ x.getNombre(), 
+                x -> Integer.valueOf(x.getFechaDeNacimiento().until(fechaActual).getYears())));
 
         System.out.println(mapa);        
     }    
